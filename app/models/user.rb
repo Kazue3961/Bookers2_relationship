7b336fb968533860ec365ff586c42c75350514fd
vagrant <vagrant@localhost.localdomain>
2020-09-06 09:44:45 +0000

    いいねコメント機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  attachment :profile_image
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+  attachment :profile_image, destroy: false
 
   validates :name, presence: true, length: {minimum:2,maximum:20}
   validates :introduction, length: {maximum:50}
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
-
   root to: 'home#top'
 
   get 'home/top' => 'home#top'
   get 'home/about'
+
+  resources :users, only: [:show, :index, :edit, :update]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
+  	resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
