4a99d2e6e3a585f262496b6db9453bd58021dc9e
vagrant <vagrant@localhost.localdomain>
2020-09-08 09:44:28 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,13 @@ Rails.application.routes.draw do
   get 'home/top' => 'home#top'
   get 'home/about'
 
-  resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
+
+  resources :books do
   	resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
